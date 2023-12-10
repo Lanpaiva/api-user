0427fb8a4274a6f83fb5d3e532d1888b73a9cd14
Lanpaiva <alanpaiva@mail.io>
2023-12-10 15:02:56 -0300

    chore: dto usecase input

--- internal/usecase/dto.go
+++ internal/usecase/dto.go
@@ -1,5 +1,10 @@
 package usecase
 
+import (
+	"github.com/lanpaiva/api-user/internal/entity"
+	"github.com/lanpaiva/api-user/internal/infra/database"
+)
+
 type CreateTaskInput struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
@@ -14,4 +19,30 @@ type CreateUserInput struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type CreateUserOutput struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type NewUser struct {
+	UserDb database.UserInterface
+}
+
+func (n *NewUser) Create(input CreateUserInput) (*CreateUserOutput, error) {
+	user, err := entity.NewUser(input.Name, input.Email, input.Password)
+	if err != nil {
+		return nil, err
+	}
+	err = n.UserDb.Create(user)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateUserOutput{
+		Name:     user.Name,
+		Email:    user.Email,
+		Password: user.Password,
+	}, nil
 }
