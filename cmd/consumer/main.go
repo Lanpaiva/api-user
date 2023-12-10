fb149fff1fcb9f35e7c40f6c0f82bfebf53d14c8
Lanpaiva <alanpaiva@mail.io>
2023-12-10 11:58:43 -0300

    feat: user requeriments added

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -24,12 +24,23 @@ func main() {
 		panic(err)
 	}
 	db.AutoMigrate(&entity.Task{}, &entity.User{})
+
 	taskDB := database.NewTaskDB(db)
+	userDB := database.NewUserDB(db)
+
 	taskHandler := handlers.NewTaskHandler(taskDB)
+	userHandler := handlers.NewUserHandler(userDB)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	r.Post("/users", userHandler.CreateUser)
+
 	r.Post("/tasks", taskHandler.CreateTask)
+	r.Get("/tasks/{id}", taskHandler.GetTask)
+	r.Get("/tasks", taskHandler.GetAllTasks)
+	r.Put("/tasks/{id}", taskHandler.TaskUpdate)
+	r.Delete("/tasks/{id}", taskHandler.TaskDelete)
 
 	http.ListenAndServe(":8000", r)
 }
